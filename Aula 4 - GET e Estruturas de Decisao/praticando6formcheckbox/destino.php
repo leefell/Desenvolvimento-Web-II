9e92895214c05377e77d8f89b324e04c3a1a96d1
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2025-03-04 17:49:04 -0300

    praticando do checkbox

---
+++ Aula 4 - GET e Estruturas de Decisao/praticando6formcheckbox/destino.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Destino</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="mt-3">Destino</h1>
+        <hr>
+
+        <h2>Dados da requisição:</h2>
+        <div class="bg-light p-3 border rounded">
+            <pre class="text-danger"><?php print_r($_POST); ?></pre>
+        </div>
+
+        <h2>Interesses selecionados (em ordem alfabética)</h2>
+        <div class="interesses">
+            <?php
+            if (isset($_POST['gostos']) && is_array($_POST['gostos'])) {
+                $gostos = $_POST['gostos'];
+                sort($gostos);
+                echo "<ul>";
+                foreach ($gostos as $gosto) {
+                    echo "<li>" . htmlspecialchars($gosto) . "</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "<p>Não tem nada aqui!</p>";
+            }
+            ?>
+        </div>
+
+        <a href="index.html">Voltar para o formulário</a>
+    </div>
+</body>
+
+</html>
