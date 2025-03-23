7f216ff64f4dba8bfade8cc395c6bada6114da4e
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2025-03-22 22:18:52 -0300

    configs iniciais

---
+++ Trabalho I/trabalho1/sobre.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <?php require 'header.php' ?>
+    
+    <div class="container flex-grow-1">
+        <p>página sobre</p>
+    </div>
+
+    <?php require 'footer.php' ?>
+</body>
+
+</html>
