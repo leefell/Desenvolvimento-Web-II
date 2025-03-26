3d85caa6b278fdcbeb7846ba085dd780dbe032e3
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2025-03-25 22:21:40 -0300

    fix tudo

--- Trabalho I/trabalho1/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-dark text-white text-center py-3 mt-auto">
-    <p>&copy; Trabalho T1 - VTPDWE2 & VTPISRV.</p>
-    <hr>
-    <p>Alexandre Augusto dos Santos Feltrin</p>
-</footer>
